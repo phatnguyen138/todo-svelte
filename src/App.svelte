c552b2bf0aadeeba51886a2036b9b8390c8aff6a
Phat <minhphatnguyen6@gmail.com>
2024-08-05 20:27:24 +0700

    init project

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "./app.css";
+</script>
+
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.blue.100);
+  }
+</style>
